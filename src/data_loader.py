d95793bbbaf251b1ed18436b3d30a1b5ed0d62d8
Jaseem Paloth <jaseempalothjp@gmail.com>
2024-08-07 16:26:17 +0530

    chore: Update data_loader module with type annotations

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,14 +1,14 @@
 import yfinance as yf
 import pandas as pd
 
-def data_loader(ticker):
+def data_loader(ticker: str) -> pd.DataFrame:
     # Load historical data for the given ticker from Yahoo Finance
     data = yf.Ticker(ticker)
     history = data.history(period="max")
     history = history.drop(columns=['Dividends', 'Stock Splits'])
     return history
 
-def preprocess_data(data):
+def preprocess_data(data: pd.DataFrame)  -> pd.DataFrame:
     # Preprocess data for modeling
     data['Tomorrow'] = data['Close'].shift(-1)
     data['Target'] = (data['Tomorrow'] > data['Close']).astype(int)
