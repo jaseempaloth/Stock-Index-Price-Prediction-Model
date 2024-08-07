14dbe2d88e99e3dea9a828d8aa1dd50773a7dc47
Jaseem Paloth <jaseempalothjp@gmail.com>
2024-08-08 01:22:52 +0530

    chore: Add feature_engineering module and create_features function

---
+++ src/feature_engineering.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def create_features(data, horizons: list[int]) -> pd.DataFrame:
+    # Create additional features based on rolling averages and trends
+    new_predictors = []
+
+    for horizon in horizons:
+        rolling_averages = data.rollling(horizon).mean()
+        ratio_column = f'Close_Ratio_{horizon}'
+        data[ratio_column] = data['Close'] / rolling_averages['Close']
+
+        trend_column = f'Trend_{horizon}'
+        data[trend_column] = data.shift(1).rolling(horizon).sum()['Target']
+
+        new_predictors += [ratio_column, trend_column]
+
+    return data.dropna(), new_predictors
