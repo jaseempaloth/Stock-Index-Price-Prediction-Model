95d65432c45ebb1c1be68c3d47615eb6fbf09ece
Jaseem Paloth <jaseempalothjp@gmail.com>
2024-08-28 21:41:58 +0530

    chore: Refactor data_loader module and update preprocess_data function

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,14 +1,14 @@
 import yfinance as yf
 import pandas as pd
 
-def data_loader(ticker: str) -> pd.DataFrame:
+def load_data(ticker):
     # Load historical data for the given ticker from Yahoo Finance
     data = yf.Ticker(ticker)
     history = data.history(period="max")
     history = history.drop(columns=['Dividends', 'Stock Splits'])
     return history
 
-def preprocess_data(data: pd.DataFrame)  -> pd.DataFrame:
+def preprocess_data(data):
     # Preprocess data for modeling
     data['Tomorrow'] = data['Close'].shift(-1)
     data['Target'] = (data['Tomorrow'] > data['Close']).astype(int)
